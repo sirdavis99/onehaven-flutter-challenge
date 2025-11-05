04bae72a1e0789da1c5539a0108520e4ca9fce1c
Friday Davis <55281305+sirdavis99@users.noreply.github.com>
2025-11-05 14:16:19 +0100

    feat: rebrand app from ride_hailing to OneHaven
    - Updated Android app label to OneHaven
    - Removed unused Rubik font family assets
    - Removed unused image and SVG assets to reduce app size

--- lib/domain/repositories/auth.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:ride_hailing/domain/entities/auth.dart';
-import 'package:ride_hailing/domain/entities/failure.dart';
-
-abstract class BaseAuthRepository {
-  Future<Either<Failure, AuthEntity>> auth(String token);
-}
